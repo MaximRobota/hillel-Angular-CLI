7a6a2cb2f1d595dc228a04ee9ac4b6a8198246c6
Maxim Robota <maxim1106@mail.ru>
2017-10-18 22:49:23 +0600

    directive 1t

--- src/app/Pages/Directive/highlight.directive.ts
+++ src/app/Pages/Directive/highlight.directive.ts
@@ -1,19 +1,22 @@
-import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({ selector: '[highlightColor]' })
 
-export class HighlightDirective   {
+export class HighlightDirective implements OnInit {
   @Input() highlightColor: string;
-  constructor(private el: ElementRef) { }
-  @HostListener('mouseenter') onMouseEnter() {
+  constructor(private el: ElementRef) {
+  }
+  ngOnInit() {
     this.highlight('yellow');
-  }
 
-  @HostListener('mouseleave') onMouseLeave() {
-    this.highlight(null);
   }
-
   private highlight(color: string) {
+    console.log();
+    let num = parseInt(this.el.nativeElement.innerHTML.replace(/\D+/g,""));
+    let text = this.el.nativeElement.innerHTML.replace(/\d/g, '');
     this.el.nativeElement.style.backgroundColor = color;
+    var div = document.createElement('div');
+    div.innerHTML = text + "<strong> " + num + "</strong>";
+    document.body.appendChild(div);
   }
 }
